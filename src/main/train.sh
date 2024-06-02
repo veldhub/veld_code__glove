91f7c36f4a3ed08cfb760c598eb54958fed0b486
SteffRhes <steffrhes@pm.me>
2024-06-02 17:20:24 +0200

    rename src/main/demo.sh to train.sh, and modify it so that it loads the variables from the environment. Also modify veld_train.yaml accordingly

---
+++ src/main/train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo "$ /opt/glove/build/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE"
+/opt/glove/build/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+
+echo "$ /opt/glove/build/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE"
+/opt/glove/build/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+
+echo "$ /opt/glove/build/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE"
+/opt/glove/build/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+
+echo "$ /opt/glove/build/glove -save-file $VECTOR_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE"
+/opt/glove/build/glove -save-file $VECTOR_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
+
+echo "$ python3 eval/python/evaluate.py"
+python3 /opt/glove/eval/python/evaluate.py
