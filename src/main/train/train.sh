6f96950828708f1b2ffd195e4c0c49b501a0ab33
SteffRhes <steffrhes@pm.me>
2024-06-12 20:34:55 +0200

    rename environment variable in_corpus_file

--- src/main/train/train.sh
+++ src/main/train/train.sh
@@ -2,7 +2,7 @@
 
 # this script is a slimmed down and adapted version of the original demo.sh script in the GloVe repo
 
-in_corpus="/veld/input/${in_corpus}"
+in_corpus_file="/veld/input/${in_corpus_file}"
 out_vocab_file="/veld/output/${out_vocab_file}"
 out_cooccurrence_file="/veld/output/${out_cooccurrence_file}"
 out_cooccurrence_shuf_file="${out_cooccurrence_shuf_file}"
@@ -11,11 +11,11 @@ out_vector_file="${out_vector_file}"
 set -e
 
 echo
-echo "$ /opt/glove/build/vocab_count -min-count $vocab_min_count -verbose $verbose < $in_corpus > $out_vocab_file"
-/opt/glove/build/vocab_count -min-count $vocab_min_count -verbose $verbose < $in_corpus > $out_vocab_file
+echo "$ /opt/glove/build/vocab_count -min-count $vocab_min_count -verbose $verbose < $in_corpus_file > $out_vocab_file"
+/opt/glove/build/vocab_count -min-count $vocab_min_count -verbose $verbose < $in_corpus_file > $out_vocab_file
 
-echo "$ /opt/glove/build/cooccur -memory $memory -vocab-file $out_vocab_file -verbose $verbose -window-size $window_size < $in_corpus > $out_cooccurrence_file"
-/opt/glove/build/cooccur -memory $memory -vocab-file $out_vocab_file -verbose $verbose -window-size $window_size < $in_corpus > $out_cooccurrence_file
+echo "$ /opt/glove/build/cooccur -memory $memory -vocab-file $out_vocab_file -verbose $verbose -window-size $window_size < $in_corpus_file > $out_cooccurrence_file"
+/opt/glove/build/cooccur -memory $memory -vocab-file $out_vocab_file -verbose $verbose -window-size $window_size < $in_corpus_file > $out_cooccurrence_file
 
 echo "$ /opt/glove/build/shuffle -memory $memory -verbose $verbose < $out_cooccurrence_file > $out_cooccurrence_shuf_file"
 /opt/glove/build/shuffle -memory $memory -verbose $verbose < $out_cooccurrence_file > $out_cooccurrence_shuf_file
